bf5579a34abe60ffaa70a98b67e8cb4942c78d94
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-05-06 11:36:49 -0300

    Implementação da Bala

--- Scripts/jogador.gd
+++ Scripts/jogador.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-
 var direcao = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	mover()
-
+	rotacionar_corpo()
+	
 	move_and_slide()
 
 func mover():
 	
 	direcao = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direcao * SPEED
+	
+func rotacionar_corpo():
+	look_at(get_global_mouse_position())
+	
 	
 	
 	
