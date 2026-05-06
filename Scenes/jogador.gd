5d8d61d5b934512677f17662bee89a31f3fb20cc
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-05-06 09:42:43 -0300

    Codigo de movimento

---
+++ Scenes/jogador.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var direcao = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	mover()
+
+	move_and_slide()
+
+func mover():
+	
+	direcao = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direcao * SPEED
+	
+	
+	
+	
