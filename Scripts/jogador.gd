49b5756027cb7dc0a5b921c0e3cc6da81b92d33a
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-05-12 09:04:53 -0300

    Implementação da Bala

--- Scripts/jogador.gd
+++ Scripts/jogador.gd
@@ -14,6 +14,8 @@ func mover():
 	direcao = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direcao * SPEED
 	
+	
+	
 func rotacionar_corpo():
 	look_at(get_global_mouse_position())
 	
