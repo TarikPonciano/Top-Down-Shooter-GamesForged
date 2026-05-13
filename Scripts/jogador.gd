96878d94efa8b05bebab1a2b2aa4edbb8ca8ffe4
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-05-13 09:27:27 -0300

    Implementação Bala

--- Scripts/jogador.gd
+++ Scripts/jogador.gd
@@ -4,16 +4,18 @@ const SPEED = 300.0
 var direcao = Vector2.ZERO
 
 @onready var ponta_arma = $PontaDaArma
+
 @export var bala_cena: PackedScene
 
 func _physics_process(delta: float) -> void:
+	
 	mover()
 	rotacionar_corpo()
 	
 	move_and_slide()
 	
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("atirar"):
 		disparar()
 		
 func disparar():
@@ -23,16 +25,15 @@ func disparar():
 	
 	#2. Configurar a bala (Posição Inicial e Direção)
 	nova_bala.global_position = ponta_arma.global_position
+	nova_bala.direcao = (ponta_arma.global_position - global_position).normalized()
 	
 	#3. Adicionar a bala na fase
 	get_tree().current_scene.add_child(nova_bala)
 
 func mover():
 	
-	direcao = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direcao = Input.get_vector("andar_esquerda", "andar_direita", "andar_cima", "andar_baixo")
 	velocity = direcao * SPEED
-	
-	
 	
 func rotacionar_corpo():
 	look_at(get_global_mouse_position())
