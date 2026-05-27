e937483c3d76adc18049bf9415acf33a8f8e806c
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-05-27 08:51:43 -0300

    Update fase_1.gd

--- Scripts/fase_1.gd
+++ Scripts/fase_1.gd
@@ -20,5 +20,28 @@ func spawn_simples():
 	#3. Spawnar inimigo na fase
 	get_tree().current_scene.add_child(novo_inimigo)
 
+func spawn_avancado():
+	var distancia_minima = 200
+	var distancia_maxima = 500
+	
+	var novo_inimigo = cena_inimigo.instantiate()
+	
+	#Lógica de Spawn Complexa
+	while true:
+		var pos_x = randi_range(-distancia_maxima, distancia_maxima)
+		var pos_y = randi_range(-distancia_maxima, distancia_maxima)
+		
+		var pos_final = Vector2(pos_x, pos_y)
+		
+		if pos_final.distance_to(Vector2.ZERO) >= distancia_minima and pos_final.distance_to(Vector2.ZERO) <= distancia_maxima:
+			novo_inimigo.global_position = global_position + pos_final
+			break
+			
+		novo_inimigo.jogador = jogador
+		
+		get_tree().current_scene.add_child(novo_inimigo)
+			
+	
+
 func _on_spawn_timer_timeout() -> void:
 	spawn_simples()
