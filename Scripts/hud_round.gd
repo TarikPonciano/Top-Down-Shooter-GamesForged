ddb9dd49d6056dfde8bcadc8b8ec21182b6a6532
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-06-09 10:59:42 -0300

    Update hud_round.gd

--- Scripts/hud_round.gd
+++ Scripts/hud_round.gd
@@ -1,12 +1,31 @@
 extends CanvasLayer
 
-var tempoMaximo = 120
+var tempoMaximo = 61
 var tempoPassado = 0
 @onready var tempo_round = $"Tempo do Round"
 
+func _ready() -> void:
+	atualizar_relogio()
+
 func _on_cronometro_round_timeout() -> void:
 	tempoPassado += 1
-	var tempoExibir = tempoMaximo - tempoPassado
+	atualizar_relogio()
+
+func atualizar_relogio():
+	var diferenca_tempo = tempoMaximo - tempoPassado
 	
-	tempo_round.text = str(tempoExibir)
+	var tempo_minutos = int(diferenca_tempo / 60)
+	var tempo_segundos = diferenca_tempo % 60
+	
+	if tempo_minutos < 10:
+		tempo_minutos = "0"+str(tempo_minutos)
+	else:
+		tempo_minutos = str(tempo_minutos)
+		
+	if tempo_segundos < 10:
+		tempo_segundos = "0"+str(tempo_segundos)
+	else:
+		tempo_segundos = str(tempo_segundos)
+	
+	tempo_round.text = tempo_minutos+":"+tempo_segundos
 	
